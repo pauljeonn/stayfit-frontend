17443f0f227094dedcac68e67876b3102e5b14fc
Paul Jeon <49374104+pauljeonn@users.noreply.github.com>
2022-01-17 14:25:11 +0900

    Feat: Clock 기능 추가

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,10 +1,54 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import dayjs from 'dayjs';
 
-const Container = styled.div``;
+const Container = styled.div`
+	width: 100%;
+	height: 100vh;
+	background-color: pink;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+
+const Wrapper = styled.div`
+	width: 500px;
+	height: 500px;
+	background-color: lightblue;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
+
+const ItemClock = styled.div`
+	font-size: 30px;
+	color: white;
+`;
 
 const MainPage = () => {
-	return <Container>MAIN</Container>;
+	const [date, setDate] = useState(new Date());
+
+	const refreshClock = () => {
+		setDate(new Date());
+	};
+
+	useEffect(() => {
+		// 1분에 한번씩 refreshClock 호출
+		const timerId = setInterval(refreshClock, 1000 * 60);
+		return () => {
+			clearInterval(timerId);
+		};
+	}, []);
+
+	return (
+		<Container>
+			<Wrapper>
+				<h1>MAIN</h1>
+				{/* dayjs 활용하여 시간 포맷 설정하기 */}
+				<ItemClock>{dayjs(date).format('h:mm A')}</ItemClock>
+			</Wrapper>
+		</Container>
+	);
 };
 
 export default MainPage;
