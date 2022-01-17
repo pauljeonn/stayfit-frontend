f58cebffbd11cc3ab41ee43592afac5993e3c245
Paul Jeon <49374104+pauljeonn@users.noreply.github.com>
2022-01-17 11:36:45 +0900

    create MainPage and ExercisePage components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import ExercisePage from './pages/ExercisePage';
 
 function App() {
-	return <div className="App">STAYFIT</div>;
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<MainPage />} />
+					<Route path="/exercise" element={<ExercisePage />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
